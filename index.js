45004a25a0e95d9ba6f90a4efb6e5f4af4ea2387
westhecool <westhecool@users.noreply.github.com>
2024-12-14 12:54:58 -0500

    switch to bun and rewrite tracker to use websockets

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const http = require('http');
+// Run with bunjs (https://bun.sh)
+const { EventEmitter } = require('events');
 const { version } = require('./package.json');
 const corsHeaders = {
     'Access-Control-Allow-Origin': '*',
@@ -7,124 +8,79 @@ const corsHeaders = {
     'Access-Control-Allow-Headers': '*',
     'Access-Control-Expose-Headers': '*',
 };
-var peers = {};
-function cleanUpPeers() {
-    for (const peer of Object.keys(peers)) {
-        if (Date.now() - peers[peer].lastSeen > 60000) { // 1 minute
-            delete peers[peer];
-        }
-    }
+const ecodes = {
+    UPGRADE_REQUIRED: 0,
+    UKNOWN_EVENT: 1,
+    INVALID_DATA: 2,
+    HOSTNAME_LOCKED: 3
 }
-setInterval(cleanUpPeers, 10000);
-const server = http.createServer((req, res) => {
-    if (req.url === '/version') {
-        res.writeHead(200, { 'Content-Type': 'application/json', ...corsHeaders });
-        res.end(JSON.stringify({ version }));
-    } else if (req.method === 'OPTIONS') {
-        res.writeHead(200, corsHeaders);
-        res.end();
-    } else if (req.method === 'POST') {
-        let body = Buffer.alloc(0);
-        req.on('data', (chunk) => {
-            body = Buffer.concat([body, chunk]);
-        });
-        req.once('end', () => {
-            try {
-                if (req.url === '/find-peers') {
-                    const files = JSON.parse(body.toString());
-                    if (!Array.isArray(files) || files.length === 0) {
-                        res.writeHead(400, { 'Content-Type': 'application/json', ...corsHeaders });
-                        res.end(JSON.stringify({ error: 'No files provided to find peers for' }));
-                        return;
-                    }
-                    let result = {};
-                    for (const file of files) {
-                        result[file] = [];
+const peerEvents = new EventEmitter();
+peerEvents.setMaxListeners(0);
+var peers = {};
+const server = Bun.serve({
+    port: process.env.PORT || 3000,
+    fetch(req, server) {
+        const remoteAddress = server.requestIP(req).address;
+        if (server.upgrade(req, { headers: corsHeaders, data: { hostname: null, lookingFor: [], remoteAddress, onpeer: null } })) {
+            return; // do not return a Response
+        }
+        const url = '/' + req.url.split('://')[1].split('/').slice(1).join('/'); // The url is a full url for some reason
+        if (url === '/version') {
+            return new Response(JSON.stringify({ version }), { status: 200, headers: { 'Content-Type': 'application/json', ...corsHeaders } });
+        } else {
+            return new Response(JSON.stringify({ error: 'Upgrade Required', ecode: ecodes.UPGRADE_REQUIRED }), { status: 426, headers: { 'Content-Type': 'text/plain', ...corsHeaders } });
+        }
+    },
+    websocket: {
+        message(ws, data) {
+            const message = JSON.parse(data);
+            if (message.event === 'ping') {
+                ws.send(JSON.stringify({ event: 'pong' }));
+            } else if (message.event === 'version') {
+                ws.send(JSON.stringify({ event: 'version', version }));
+            } else if (message.event === 'find-peers') {
+                if (Array.isArray(message.files)) {
+                    ws.data.lookingFor = message.files;
+                    // Look for any existing peers
+                    for (const file of ws.data.lookingFor) {
                         for (const peer of Object.keys(peers)) {
                             if (peers[peer].files.includes(file)) {
-                                result[file].push(peer);
+                                ws.send(JSON.stringify({ event: 'peer', fileHash: file, peer: peer }));
                             }
                         }
                     }
-                    res.writeHead(200, { 'Content-Type': 'application/json', ...corsHeaders });
-                    res.end(JSON.stringify(result));
-                } else if (req.url === '/announce-files') {
-                    const data = JSON.parse(body.toString());
-                    const remoteAddress = req.socket.remoteAddress;
-                    if (!Array.isArray(data.files) || !data.hostname) {
-                        res.writeHead(400, { 'Content-Type': 'application/json', ...corsHeaders });
-                        res.end(JSON.stringify({ error: 'Invalid data' }));
-                        return;
-                    }
-                    if (peers[data.hostname] && peers[data.hostname].remoteAddress !== remoteAddress) {
-                        res.writeHead(403, { 'Content-Type': 'application/json', ...corsHeaders });
-                        res.end(JSON.stringify({ error: 'Peer is locked to another address' }));
-                        return;
-                    }
-                    peers[data.hostname] = {
-                        remoteAddress,
-                        files: data.files,
-                        lastSeen: Date.now()
+                    ws.data.onpeer = (peer) => {
+                        for (const file of ws.data.lookingFor) {
+                            if (peers[peer].files.includes(file)) {
+                                ws.send(JSON.stringify({ event: 'peer', fileHash: file, peer: peer }));
+                            }
+                        }
                     };
-                    res.writeHead(200, { 'Content-Type': 'application/json', ...corsHeaders });
-                    res.end(JSON.stringify({ success: true }));
-                } else if (req.url === '/ping') {
-                    const data = JSON.parse(body.toString());
-                    const remoteAddress = req.socket.remoteAddress;
-                    if (!data.hostname) {
-                        res.writeHead(400, { 'Content-Type': 'application/json', ...corsHeaders });
-                        res.end(JSON.stringify({ error: 'Invalid hostname' }));
-                        return;
-                    }
-                    if (peers[data.hostname]) {
-                        if (peers[data.hostname].remoteAddress !== remoteAddress) {
-                            res.writeHead(403, { 'Content-Type': 'application/json', ...corsHeaders });
-                            res.end(JSON.stringify({ error: 'Peer is locked to another address' }));
-                            return;
-                        }
-                        peers[data.hostname].lastSeen = Date.now();
-                        res.writeHead(200, { 'Content-Type': 'application/json', ...corsHeaders });
-                        res.end(JSON.stringify({ success: true }));
-                    } else {
-                        res.writeHead(400, { 'Content-Type': 'application/json', ...corsHeaders });
-                        res.end(JSON.stringify({ error: 'Peer not found' }));
-                    }
-                } else if (req.url === '/disconnect') {
-                    const data = JSON.parse(body.toString());
-                    const remoteAddress = req.socket.remoteAddress;
-                    if (!data.hostname) {
-                        res.writeHead(400, { 'Content-Type': 'application/json', ...corsHeaders });
-                        res.end(JSON.stringify({ error: 'Invalid hostname' }));
-                        return;
-                    }
-                    if (peers[data.hostname]) {
-                        if (peers[data.hostname].remoteAddress !== remoteAddress) {
-                            res.writeHead(403, { 'Content-Type': 'application/json', ...corsHeaders });
-                            res.end(JSON.stringify({ error: 'Peer is locked to another address' }));
-                            return;
-                        }
-                        delete peers[data.hostname];
-                        res.writeHead(200, { 'Content-Type': 'application/json', ...corsHeaders });
-                        res.end(JSON.stringify({ success: true }));
-                    } else {
-                        res.writeHead(400, { 'Content-Type': 'application/json', ...corsHeaders });
-                        res.end(JSON.stringify({ error: 'Peer not found' }));
-                    }
+                    peerEvents.addListener('peer', ws.data.onpeer);
                 } else {
-                    res.writeHead(404, { 'Content-Type': 'application/json', ...corsHeaders });
-                    res.end(JSON.stringify({ error: 'Unknown route' }));
+                    ws.send(JSON.stringify({ event: 'error', ecode: ecodes.INVALID_DATA }));
                 }
-            } catch (error) {
-                console.error(error);
-                res.writeHead(500, { 'Content-Type': 'application/json', ...corsHeaders });
-                res.end(JSON.stringify({ error: 'Internal Server Error' }));
+            } else if (message.event === 'announce-files') {
+                if (Array.isArray(message.files) && message.hostname) {
+                    if (peers[data.hostname] && peers[data.hostname].remoteAddress !== ws.data.remoteAddress) {
+                        ws.send(JSON.stringify({ event: 'error', ecode: ecodes.HOSTNAME_LOCKED }));
+                        return;
+                    }
+                    ws.data.hostname = message.hostname;
+                    peers[ws.data.hostname] = {
+                        remoteAddress: ws.data.remoteAddress,
+                        files: message.files
+                    };
+                    peerEvents.emit('peer', ws.data.hostname);
+                } else {
+                    ws.send(JSON.stringify({ event: 'error', ecode: ecodes.INVALID_DATA }));
+                }
             }
-        });
-    } else {
-        res.writeHead(405, { 'Content-Type': 'application/json', ...corsHeaders });
-        res.end(JSON.stringify({ error: 'Method Not Allowed' }));
+        },
+        close(ws, code, message) {
+            if (ws.data.onpeer) peerEvents.removeListener('peer', ws.data.onpeer);
+            if (ws.data.hostname) delete peers[ws.data.hostname];
+        }
     }
 });
-server.listen(process.env.PORT || 3000, () => {
-    console.log(`Server running at http://localhost:${process.env.PORT || 3000} (http://0.0.0.0:${process.env.PORT || 3000})`);
-});
+console.log(`Server running at http://localhost:${process.env.PORT || 3000} (http://0.0.0.0:${process.env.PORT || 3000})`);
